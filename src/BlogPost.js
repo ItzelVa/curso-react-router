ad93a77768f1f2d7eaa4151baa421e5655e2d916
ItzelVa <itzelochoavallejo@gmail.com>
2024-03-11 12:50:39 -0600

    Implementation of useParams and useNavigation in the BlogPage and BlogPost

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,15 +1,23 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { blogdata } from "./blogdata"
 
 export function BlogPost () {
 
     const {slug} = useParams()
 
+    const navigate = useNavigate()
+
     const blogpost = blogdata.find(post => post.slug === slug)
+
+    const returnToBlog = () => {
+        navigate('/blog')
+        // navigate(-1) sirve para hacer un back a la ruta anterior
+    }
 
     return (
         <>
             <h2>{blogpost.title}</h2>
+            <button onClick={returnToBlog}>Volver al blog</button>
             <p>{blogpost.content}</p>
             <p>{blogpost.author}</p>
         </>
